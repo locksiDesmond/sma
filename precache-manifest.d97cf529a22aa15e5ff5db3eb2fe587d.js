29e801b80feedd461f833fb479420a72beaf164f
LocksiDesmond <Des.olanrewaju@gmail.com>
2020-07-18 22:12:05 +0100

    Updates

---
+++ precache-manifest.d97cf529a22aa15e5ff5db3eb2fe587d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af568e32ae749853a639b521c23b385e",
+    "url": "/SMA/index.html"
+  },
+  {
+    "revision": "ad5acca9cda770b266c0",
+    "url": "/SMA/static/css/main.a6893cc1.chunk.css"
+  },
+  {
+    "revision": "36acf8aa1a626639f853",
+    "url": "/SMA/static/js/2.13ae6e4a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SMA/static/js/2.13ae6e4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad5acca9cda770b266c0",
+    "url": "/SMA/static/js/main.6704d0c3.chunk.js"
+  },
+  {
+    "revision": "b2c8eb8781f62de384ff",
+    "url": "/SMA/static/js/runtime-main.b455d411.js"
+  },
+  {
+    "revision": "548ac777a7da7f810a7f695b7ba1fd5d",
+    "url": "/SMA/static/media/welcome.548ac777.svg"
+  }
+]);
